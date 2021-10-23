fae9f7a09e9cf9167ae841aff5760d1a10705195
zachinshakya <sachinshakya507@gmail.com>
2021-10-23 08:37:03 +0545

    ms

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node! js. I am in sachin branch'
+  const msg = 'Hello Node! js. I am in sachin branch.To devops'
   res.end(msg);
 });
 
