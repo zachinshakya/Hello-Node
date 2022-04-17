e6a68219be332f4a220c60e1a9be097cf4bfbb57
zachinshakya <sachinshakya507@gmail.com>
2022-04-17 10:41:46 +0545

    message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node sachin gitops'
+  const msg = 'Hello Node sachin gitops sachin'
   
   res.end(msg);
 });
