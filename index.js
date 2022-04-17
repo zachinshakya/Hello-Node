1e276f082ef5b1056b948ebf18cbba80f323254e
zachinshakya <sachinshakya507@gmail.com>
2022-04-17 19:11:15 +0545

    version

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello sachin Devops123'
+  const msg = 'Hello sachin Devops123 version1.0.0'
   
   res.end(msg);
 });
